9b86900b9b43a19cd9dc1be1a56e8ba45e477da1
callistus406 <callistus406@gmail.com>
2022-12-20 16:22:45 +0100

    user context provider

--- src/context/UserContextProvider.jsx
+++ src/context/UserContextProvider.jsx
@@ -1,12 +1,15 @@
 import { useState, createContext, useEffect } from 'react';
 
-function UserContextProvider(props) {
-  const userContext = createContext();
-  const [userData, setUserData] = useState();
+export const UserContext = createContext();
+export function UserContextProvider(props) {
+  // console.log(props);
+  const [userData, setUserData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     let isCancelled = false;
-
+    setLoading(true);
     fetch('https://jsonplaceholder.typicode.com/users', {
       headers: {
         'Content-Type': 'application/json',
@@ -19,18 +22,37 @@ function UserContextProvider(props) {
       })
       .then((data) => {
         if (!isCancelled) {
+          setLoading(false);
+          // console.log(data);
           setUserData(data);
+          setError('');
         }
       })
       .catch((error) => {
-        console.log(error);
+        setLoading(false);
+        setError(error.message);
+        setUserData([]);
+        console.log(error.message);
       });
+
     return () => {
       isCancelled = true;
     };
   }, []);
 
-  return <userContext.Provider>{props.children}</userContext.Provider>;
+  return (
+    <>
+      {loading ? (
+        <h1>Loading</h1>
+      ) : error ? (
+        <h1>error</h1>
+      ) : (
+        <UserContext.Provider
+          value={{ data: [userData, setUserData], dataState: [loading, error] }}
+        >
+          {props.children}
+        </UserContext.Provider>
+      )}
+    </>
+  );
 }
-
-export default UserContextProvider;
